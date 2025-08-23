ca4d4ed82d216af631fc78d1c48d7834bdde4914
Alex <alejandro.antolin.92@gmail.com>
2025-08-23 15:24:47 +0200

    Add transitions and animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingComponent } from './features/landing/landing.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { ServicesComponent } from './features/services/services.component';
+import { ContactComponent } from './features/contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingComponent, ServicesComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
