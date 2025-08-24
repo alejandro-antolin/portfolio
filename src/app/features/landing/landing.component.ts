efd44438f10cdfde4b1c6c6e52a707ae418b9cdf
Alex <alejandro.antolin.92@gmail.com>
2025-08-24 20:19:37 +0200

    About me structure and styling

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -1,9 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { lucideArrowDown, lucideArrowRight, lucideGithub, lucideInstagram, lucideLinkedin, lucideScrollText } from '@ng-icons/lucide';
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage, NgIconComponent],
+  providers: [provideIcons({ lucideInstagram, lucideGithub, lucideLinkedin , lucideArrowDown })],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
